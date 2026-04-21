bbbe4405eb2bf00e58a85c41b5ee9de304c4f485
Eliza Sikira <sikiraeliza2@gmail.com>
2026-04-22 01:43:17 +0200

    feat: isPseudo method

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -3,11 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   ScalarConverter.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: elsikira <elsikira@student.42.fr>          +#+  +:+       +#+        */
+/*   By: elizasikira <elizasikira@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/22 01:12:37 by elsikira          #+#    #+#             */
-/*   Updated: 2026/04/22 01:12:38 by elsikira         ###   ########.fr       */
+/*   Updated: 2026/04/22 01:42:30 by elizasikira      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ScalarConverter.hpp"
 
+ScalarConverter::ScalarConverter(){}
+
+bool    ScalarConverter::isPseudo(const std::string& lit)
+{
+    std::string pseudo[] = {"nan", "nanf", "+inf", "+inff", "-inf", "-inff"};
+    
+}
+
+
+ScalarConverter::~ScalarConverter(){}
--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: elizasikira <elizasikira@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/22 01:15:56 by elsikira          #+#    #+#             */
-/*   Updated: 2026/04/22 01:18:54 by elizasikira      ###   ########.fr       */
+/*   Updated: 2026/04/22 01:39:54 by elizasikira      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,14 @@ int main(int argc, char **argv)
 {
     if (argc < 2)
     {
-        std::cerr << "Usage: ./ScalarConverter [literal 1] [literal 2]" << std::endl
+        std::cerr << "Usage: ./ScalarConverter [literal 1] [literal 2]" << std::endl;
+        return 1;
     }
-    
+    for (int i = 1; i < argc; i++)
+    {
+        std::cout << argv[i] << std::endl;
+        ScalarConverter::convert(argv[i]);
+        std::cout << std::endl;
+    }
     return 0;
 }
